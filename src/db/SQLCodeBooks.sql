039111228cbd0bc3a2e3736dda9e2670597f9b26
sisternia <69025607+sisternia@users.noreply.github.com>
2024-04-05 17:04:31 +0700

    crud_books

---
+++ src/db/SQLCodeBooks.sql
@@ -0,0 +1,27 @@
+﻿Create Datebase SQLBooks
+Use SQLBooks
+
+Create Table Account(
+TenDangNhap varchar(20) not null,
+MatKhau varchar(20) not null,
+ConfirmMatKhau varchar(20) not null,
+Constraint PK_Account Primary Key (TenDangNhap))
+
+Create Table Books(
+MaSach varchar(20) not null,
+TenSach nvarchar(100) not null,
+Tap float not null,
+TheLoai nvarchar(30) not null,
+PhienBan nvarchar(50) not null,
+LoaiSach nvarchar(40) not null,
+NgayPhatHanh date not null,
+LuongInAn varchar(20) not null,
+DonGia varchar(20) not null,
+NXB nvarchar(20) not null,
+HinhAnh image,
+Constraint PK_Books Primary Key (MaSach))
+Drop Table Books
+
+
+
+
---
+++ src/db/SQLDatabaseBooks.sql
@@ -0,0 +1,26 @@
+﻿Use SQLBooks
+
+Insert Into Account
+	(TenDangNhap,MatKhau,ConfirmMatKhau)
+Values
+	('admin','1','1');
+Select * From Account;
+
+
+
+Insert Into Books
+	(MaSach,TenSach,Tap,TheLoai,PhienBan,LoaiSach,NgayPhatHanh,LuongInAn,DonGia,NXB)
+Values
+	('KD-1.001',N'Thiên sứ nhà bên',1,N'Đời thường',N'Giới hạn',N'Light Novel','2021-11-11',50.000,95.000,N'Kim Đồng'),
+	('KD-1.002',N'Thiên sứ nhà bên',2,N'Đời thường',N'Giới hạn',N'Light Novel','2022-02-28',50.000,95.000,N'Kim Đồng'),
+	('KD-1.003',N'Thiên sứ nhà bên',3,N'Đời thường',N'Giới hạn',N'Light Novel','2022-05-30',50.000,95.000,N'Kim Đồng'),
+	('KD-1.004',N'Thiên sứ nhà bên',4,N'Đời thường',N'Giới hạn',N'Light Novel','2022-09-30',50.000,95.000,N'Kim Đồng'),
+	('KD-1.005',N'Thiên sứ nhà bên',5,N'Đời thường',N'Giới hạn',N'Light Novel','2022-12-26',50.000,95.000,N'Kim Đồng'),
+	('KD-1.006',N'Thiên sứ nhà bên',5.5,N'Đời thường',N'Giới hạn',N'Light Novel','2023-03-27',50.000,95.000,N'Kim Đồng'),
+	('AM-2.001',N'Date A Live',1,N'Đời thường',N'Giới hạn',N'Light Novel','2023-03-27',10.000,108.000,N'Amak Books'),
+	('AM-2.002',N'Date A Live',2,N'Đời thường',N'Giới hạn',N'Light Novel','2023-03-27',10.000,108.000,N'Amak Books'),
+	('AM-2.003',N'Date A Live',3,N'Đời thường',N'Giới hạn',N'Light Novel','2023-03-27',10.000,108.000,N'Amak Books');
+Select * From Books
+
+Delete From Books
+
